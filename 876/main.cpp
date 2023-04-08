c009e2cd780d14fabae160e01722f001504dcc59
UnciaBit <unciabit@gmail.com>
2023-04-08 12:27:55 +1000

    add 876

---
+++ 876/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        // 2 pointers, one moves 2 steps, one moves 1 step
+        ListNode *slow = head, *fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+
+    Solution s;
+
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next = new ListNode(6);
+    head->next->next->next->next->next->next = new ListNode(7);
+    head->next->next->next->next->next->next->next = new ListNode(8);
+    head->next->next->next->next->next->next->next->next = new ListNode(9);
+    head->next->next->next->next->next->next->next->next->next = new ListNode(10);
+    head->next->next->next->next->next->next->next->next->next->next = new ListNode(11);
+
+    ListNode *mid = s.middleNode(head);
+
+    std::cout << mid->val << std::endl;
+
+    return 0;
+}
